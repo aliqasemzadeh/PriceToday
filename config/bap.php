f267a0988251b0266b92977b01653646f65f331d
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-04-25 08:18:43 +0330

    for vite assets.

--- config/bap.php
+++ config/bap.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'name' => 'Base Admin Panel',
+    'name' => 'Price Today',
     'container-app' => 'container',
     'container-panel' => 'container-fluid',
     'container-admin' => 'container-fluid',
