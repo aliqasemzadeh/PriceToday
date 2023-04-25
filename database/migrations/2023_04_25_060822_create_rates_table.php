f7f8d8875af56c6182f1d96e0d5a4c1ebe86584a
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-04-25 09:40:43 +0330

    Add Rate To view much faster

---
+++ database/migrations/2023_04_25_060822_create_rates_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rates', function (Blueprint $table) {
+            $table->id();
+            $table->string('symbol');
+            $table->double('price');
+            $table->double('1h')->nullable();
+            $table->double('24h')->nullable();
+            $table->double('1w')->nullable();
+            $table->double('1n')->nullable();
+            $table->longText('options')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rates');
+    }
+};
