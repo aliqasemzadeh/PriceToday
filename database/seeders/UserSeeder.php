32afc4e34e4ea0459d6a0510d4251279a8e6ddd6
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-04-26 12:06:42 +0330

    fix seeder information

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -21,7 +21,7 @@ class UserSeeder extends Seeder
     public function run()
     {
         $user = new User();
-        $user->email = 'info@bap.local';
+        $user->email = 'info@pricetoday.ir';
         $user->password = Hash::make('P@ssw0rd321');
         $user->email_verified_at = Carbon::now();
         $user->save();
