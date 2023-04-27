dcce794aa9d47b19f5ab377b493a1790606a28f6
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-04-27 19:18:35 +0330

    update rates.

--- app/Jobs/UpdateRateJob.php
+++ app/Jobs/UpdateRateJob.php
@@ -18,6 +18,7 @@ class UpdateRateJob implements ShouldQueue
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     public $symbol;
+
     /**
      * Create a new job instance.
      */
@@ -33,29 +34,31 @@ class UpdateRateJob implements ShouldQueue
     {
         $symbols = Symbol::select(['coingecko_id'])->getArray();
         $symbolsString = join(",", $symbols);
-            $curl = curl_init();
-            curl_setopt_array($curl, array(
-                CURLOPT_URL => 'https://api.coingecko.com/api/v3/simple/price?ids=' . $symbolsString . '&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true',
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_ENCODING => '',
-                CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 0,
-                CURLOPT_FOLLOWLOCATION => true,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => 'GET',
-            ));
+        Log::info('https://api.coingecko.com/api/v3/simple/price?ids=' . $symbolsString . '&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true');
 
-            $response = curl_exec($curl);
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api.coingecko.com/api/v3/simple/price?ids=' . $symbolsString . '&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
 
-            curl_close($curl);
+        $response = curl_exec($curl);
 
-            $data = json_decode($response, true);
-            foreach ($symbols as $symbol) {
-                $symbol->price = $data[$symbol->coingecko_id]['usd'];
-                $symbol->market_cap = $data[$symbol->coingecko_id]['usd_market_cap'];
-                $symbol->vol_24h = $data[$symbol->coingecko_id]['usd_24h_vol'];
-                $symbol->change_24h = $data[$symbol->coingecko_id]['usd_24h_change'];
-                $symbol->save();
-            }
+        curl_close($curl);
+
+        $data = json_decode($response, true);
+        foreach ($symbols as $symbol) {
+            $symbol->price = $data[$symbol->coingecko_id]['usd'];
+            $symbol->market_cap = $data[$symbol->coingecko_id]['usd_market_cap'];
+            $symbol->vol_24h = $data[$symbol->coingecko_id]['usd_24h_vol'];
+            $symbol->change_24h = $data[$symbol->coingecko_id]['usd_24h_change'];
+            $symbol->save();
+        }
     }
 }
