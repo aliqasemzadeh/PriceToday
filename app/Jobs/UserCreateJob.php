92839ec01a5a1f7f133ae2e47a4ee23e11c55986
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-04-30 05:46:08 +0330

    fix observer.

--- app/Jobs/UserCreateJob.php
+++ app/Jobs/UserCreateJob.php
@@ -8,6 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use App\Models\User;
 
 class UserCreateJob implements ShouldQueue
 {
