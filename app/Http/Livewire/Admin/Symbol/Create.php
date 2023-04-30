58239d7bc6d4af05e16510737561e386d0feeb7f
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-04-30 06:01:56 +0330

    add Symbol index

---
+++ app/Http/Livewire/Admin/Symbol/Create.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Symbol;
+
+use Livewire\Component;
+
+class Create extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.symbol.create');
+    }
+}
---
+++ app/Http/Livewire/Admin/Symbol/Edit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Symbol;
+
+use Livewire\Component;
+
+class Edit extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.symbol.edit');
+    }
+}
