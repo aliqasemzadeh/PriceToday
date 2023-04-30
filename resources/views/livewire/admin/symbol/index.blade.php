0d593d52d558c9a988b8f30fa88334a21c5c77f6
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-04-30 20:44:21 +0330

    fix sortting.

--- resources/views/livewire/admin/symbol/index.blade.php
+++ resources/views/livewire/admin/symbol/index.blade.php
@@ -89,9 +89,9 @@
                             @endif
                         @endif
                     </th>
-                    <th wire:click="sortByColumn('type')">{{ __('bap.symbol') }}
+                    <th wire:click="sortByColumn('symbol')">{{ __('bap.symbol') }}
 
-                        @if ($sortColumn == 'type')
+                        @if ($sortColumn == 'symbol')
                             @if($sortDirection == 'asc')
                                 <!-- Download SVG icon from http://tabler-icons.io/i/chevron-up -->
                                 <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-sm text-dark icon-thick" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><polyline points="6 15 12 9 18 15" /></svg>
@@ -115,7 +115,7 @@
                             @endif
                         @endif
                     </th>
-                    <th></th>
+                    <th>{{ __('bap.coingecko_number') }}</th>
                     <th></th>
                 </tr>
                 </thead>
