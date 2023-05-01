d36e92f42938e028d3dbdc9aa9aaf56fc225f2a0
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-05-01 19:25:06 +0330

    design database for alert symbols.

---
+++ database/migrations/2023_05_01_155237_create_alert_symbols_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('alert_symbols', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('symbol_id');
+            $table->bigInteger('user_id');
+            $table->double('less_than')->default(0);
+            $table->double('more_than')->default(0);
+            $table->double('change_percent')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('alert_symbols');
+    }
+};
